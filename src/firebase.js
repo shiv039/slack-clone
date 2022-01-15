0b5efa74b611f0ee18ce585712e110dae9e67070
Shiva <shiv039@gmail.com>
2022-01-15 10:28:05 +0530

    Firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCr38LkN8cIi1lW605Kk_oueyFHUnX7aks",
+    authDomain: "slack-clone-162af.firebaseapp.com",
+    projectId: "slack-clone-162af",
+    storageBucket: "slack-clone-162af.appspot.com",
+    messagingSenderId: "275155217882",
+    appId: "1:275155217882:web:8eac349f4a0e0c986d24e3"
+  };
